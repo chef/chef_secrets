e24cbf1f0b50d0b2260f9b2a014d76fc9f8bb0bd
Stephan Renatus <srenatus@chef.io>
2017-02-21 17:43:47 +0100

    add many stuff in json file test
    Signed-off-by: Stephan Renatus <srenatus@chef.io>

--- test/chef_secrets_tests.erl
+++ test/chef_secrets_tests.erl
@@ -12,3 +12,13 @@ get_returns_value_if_it_exists_test() ->
     ExpectedResponse = {ok, <<"Value">>},
     ActualResponse = chef_secrets:get(<<"Name">>),
     ?assertEqual(ExpectedResponse, ActualResponse).
+
+get_returns_value_if_more_than_this_one_secret_exists_test() ->
+    application:set_env(chef_secrets, provider, chef_secrets_json_file),
+    TestFilePath = filename:join(code:priv_dir(chef_secrets),
+                                 "../test/json_secrets_file.json"),
+    application:set_env(chef_secrets, provider_config, [{other, foo}, {secrets_file, TestFilePath}]),
+    application:ensure_all_started(chef_secrets),
+    ExpectedResponse = {ok, <<"Value">>},
+    ActualResponse = chef_secrets:get(<<"Name">>),
+    ?assertEqual(ExpectedResponse, ActualResponse).

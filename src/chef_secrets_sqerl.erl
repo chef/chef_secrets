362d66e58988a137c782e0c6857350a054e2e472
Lincoln Baker <lbaker@chef.io>
2019-08-01 09:55:29 -0500

    patch config to pass in extra db options from envy

--- src/chef_secrets_sqerl.erl
+++ src/chef_secrets_sqerl.erl
@@ -10,5 +10,18 @@
 
 -spec config({binary(), binary()}) -> proplists:proplist().
 config({Section, Keyname}) ->
+    IdleCheck = envy:get(sqerl, idle_check, 1000, non_neg_integer),
+    Statements = sqerl_config_env:read_statements_from_config(),
     {ok, Password} = chef_secrets:get(Section, Keyname),
-    [{pass, Password} | sqerl_config_env:config()].
+
+    %% The ip_mode key in the sqerl clause determines if we parse db_host as IPv4 or IPv6
+    [{host, envy_parse:host_to_ip(sqerl, db_host)},
+     {port, envy:get(sqerl, db_port, pos_integer)},
+     {user, envy:get(sqerl, db_user, string)},
+     {pass, Password},
+     {db, envy:get(sqerl, db_name, string)},
+     {extra_options, envy:get(sqerl, db_options, [], list)},
+     {timeout, envy:get(sqerl,db_timeout, 5000, pos_integer)},
+     {idle_check, IdleCheck},
+     {prepared_statements, Statements},
+     {column_transforms, envy:get(sqerl, column_transforms, list)}].

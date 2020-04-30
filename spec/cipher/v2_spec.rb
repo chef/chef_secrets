175ac5baef7af3f87a3010580fa2e9bd17bb83ee
Bryan McLellan <btm@loftninjas.org>
2020-04-30 00:55:18 -0400

    Update test with limited key/iv lengths
    Ruby now rejects iv and key longer than 16 and 32 bytes, respectively.
    
    https://github.com/ruby/ruby/commit/ce635262f53b760284d56bb1027baebaaec175d1
    
    Signed-off-by: Bryan McLellan <btm@loftninjas.org>

--- spec/cipher/v2_spec.rb
+++ spec/cipher/v2_spec.rb
@@ -2,17 +2,17 @@ require "spec_helper"
 require "openssl"
 
 describe Veil::Cipher::V2 do
-  let(:iv64) { Base64.strict_encode64("mondaytuesdaywednesday") }
-  let(:key64) { Base64.strict_encode64("thursdayfridaysaturdaysundaymonday") }
-  let(:ciphertext64) { "yhHR8ZVUfzRv+4dvcUFlitdmCS3ybi2dGLofzEF1Ibw=" }
+  let(:iv64) { Base64.strict_encode64("mondaytue16bytes") }
+  let(:key64) { Base64.strict_encode64("thursdayfridaysaturdaysun32bytes") }
+  let(:ciphertext64) { "qS6SRmgOgSta2jSdD60sJmu83cRgy4DUJ2nKZwStrqs=" }
   let(:plainhash) { { "chef-server": "test-value" } }
 
   describe "#new" do
     it "accepts passed key and iv base64-encoded data" do
       cipher = described_class.new(iv: iv64, key: key64)
 
-      expect(cipher.iv).to eq("mondaytuesdaywednesday")
-      expect(cipher.key).to eq("thursdayfridaysaturdaysundaymonday")
+      expect(cipher.iv).to eq("mondaytue16bytes")
+      expect(cipher.key).to eq("thursdayfridaysaturdaysun32bytes")
     end
 
     it "generates key and iv data if none was passed" do
@@ -36,8 +36,8 @@ describe Veil::Cipher::V2 do
   describe "#to_hash" do
     it "base64-encodes iv and key" do
       expected = {
-        iv: "bW9uZGF5dHVlc2RheXdlZG5lc2RheQ==",
-        key: "dGh1cnNkYXlmcmlkYXlzYXR1cmRheXN1bmRheW1vbmRheQ==",
+        iv: "bW9uZGF5dHVlMTZieXRlcw==",
+        key: "dGh1cnNkYXlmcmlkYXlzYXR1cmRheXN1bjMyYnl0ZXM=",
         type: "Veil::Cipher::V2"
       }
       expect(described_class.new(iv: iv64, key: key64).to_hash).to eq(expected)

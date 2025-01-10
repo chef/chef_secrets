fb2d45e251c7b351aa14bac4f5cc31fa71a42c35
jan shahid shaik <jashaik@progress.com>
2025-01-10 11:53:49 +0530

    Fix for TLS verify_ca verify mode - PR #110 solution
    Signed-off-by: jan shahid shaik <jashaik@progress.com>

--- src/chef_secrets_sqerl.erl
+++ src/chef_secrets_sqerl.erl
@@ -13,6 +13,8 @@ config({Section, Keyname}) ->
     IdleCheck = envy:get(sqerl, idle_check, 1000, non_neg_integer),
     Statements = sqerl_config_env:read_statements_from_config(),
     {ok, Password} = chef_secrets:get(Section, Keyname),
+    DbOptions = envy:get(sqerl, db_options, [], list),
+    DbOptions1 = sqerl_config_env:handle_custom_ssl_options(DbOptions),
 
     %% The ip_mode key in the sqerl clause determines if we parse db_host as IPv4 or IPv6
     [{host, envy_parse:host_to_ip(sqerl, db_host)},
@@ -20,7 +22,7 @@ config({Section, Keyname}) ->
      {user, envy:get(sqerl, db_user, string)},
      {pass, Password},
      {db, envy:get(sqerl, db_name, string)},
-     {extra_options, envy:get(sqerl, db_options, [], list)},
+     {extra_options, DbOptions1},
      {timeout, envy:get(sqerl,db_timeout, 5000, pos_integer)},
      {idle_check, IdleCheck},
      {prepared_statements, Statements},
